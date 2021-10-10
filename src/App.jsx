60e3ff80e45ec9375813d3847bba49f257a39d7d
Oscar Clavijo <oscar.clavijo90@gmail.com>
2021-10-09 19:47:49 -0500

    Se agrega el Logo a la parte superior de la página

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,21 @@
-import logo from './logo.svg';
 import './styles/styles.css';
 import tennisBlancos from "./media/Blancos.jpg"
 import tennisNegros from "./media/Negros.jpg"
+import logo from "./media/Tech&Fun.png"
 
 function App() {
   return (
     <div className="App">
   <header>
-      
+
   </header>
   <main>
      <ul className="navbar">
+     <li>
+        <div className="logoPpal">
+          <img src={logo} alt="" />          
+        </div>
+      </li>
       <li>
           <button className="Button mainButton">Agregar Producto</button>
       </li>
@@ -20,8 +25,8 @@ function App() {
               <i className="fas fa-search logoBusqueda"></i>
           </div>
       </li>
-      <li>Iniciar Sesión</li>
       <li><button className="Button secondaryButton">Editar Producto</button></li>
+      
   </ul>
   <section>
       <h2>PRODUCTOS</h2>
