a08a1664eaff15fb3599bca38719b70bf4aba49e
Oscar Clavijo <oscar.clavijo90@gmail.com>
2021-10-10 19:41:47 -0500

    Se crea carpeta components

--- src/App.jsx
+++ src/App.jsx
@@ -42,13 +42,4 @@ function App() {
   );
 }
 
-function TennisDeportivos({imagen,Referencia}){
-  return (
-  <li className="espacioEntreImagenes">
-  <div className="contenedorImagen">
-    <img src= {imagen} alt= "Alt"/>
-   </div>
-  <span className="estiloNombre"> {Referencia}</span>       
-</li>)
-}
 export default App;
---
+++ src/components/TennisDeportivos.jsx
@@ -0,0 +1,9 @@
+function TennisDeportivos({imagen,Referencia}){
+    return (
+    <li className="espacioEntreImagenes">
+    <div className="contenedorImagen">
+      <img src= {imagen} alt= "Alt"/>
+     </div>
+    <span className="estiloNombre"> {Referencia}</span>       
+  </li>)
+}
