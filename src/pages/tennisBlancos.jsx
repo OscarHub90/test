56b25e5c86dae4cc87a2d9382b3736b10ff14ab3
Oscar Clavijo <oscar.clavijo90@gmail.com>
2021-10-13 22:36:40 -0500

    Se da por finalizado el proyecto en su fase inicial

--- src/pages/tennisBlancos.jsx
+++ src/pages/tennisBlancos.jsx
@@ -1,7 +1,11 @@
+import Footer from "components/Footer";
 
 function TennisBlancos (){
     return(
+        <div>
         <div>Esta es la página informativa para los tennis blancos.</div>
+        <Footer />
+        </div>
     )
 }
 export default TennisBlancos;
